c209a7a686881bce3797951f746b7e2b44801af5
zswfade <1912945691@qq.com>
2023-12-19 20:03:18 +0800

    这是一个命令行运行java的测试

---
+++ A.java
@@ -0,0 +1,18 @@
+/*
+ *  编译文件：javac A.class -encoding UTF-8
+ *  运行文件：java A
+ *  String str=”你好“；
+ *  String str1=new String(str.getbytes("utf-8"),"gbk");//将utf-8转成gbk码
+ *
+ * */
+public class A{
+
+    public static void main(String [] args){
+
+
+System.out.println("你好");
+    }
+
+
+
+}
